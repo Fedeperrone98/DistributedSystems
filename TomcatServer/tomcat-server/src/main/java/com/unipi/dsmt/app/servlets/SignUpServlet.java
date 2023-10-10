9e11874a313ac8b7c87abbd3571cb2d9bd15f501
Fedeperrone98 <f.perrone6@studenti.unipi.it>
2023-10-10 17:55:08 +0200

    Update SignUpServlet.java

--- TomcatServer/tomcat-server/src/main/java/com/unipi/dsmt/app/servlets/SignUpServlet.java
+++ TomcatServer/tomcat-server/src/main/java/com/unipi/dsmt/app/servlets/SignUpServlet.java
@@ -1,9 +1,22 @@
 package com.unipi.dsmt.app.servlets;
 
+import java.io.IOException;
+
+import com.unipi.dsmt.app.utils.AccessController;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "SignUp", value = "/signup")
 public class SignUpServlet extends HttpServlet {
-    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+        String targetJSP = "/WEB-INF/jsp/signup.jsp";
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher(targetJSP);
+		requestDispatcher.forward(request, response);
+    }
 }
