14d70b0c11f9b1ee488723dfce59a0d955858499
dgl1797 <digiacomoluca1797@gmail.com>
2023-10-11 19:44:25 +0200

    fixed

--- TomcatServer/tomcat-server/src/main/java/com/unipi/dsmt/app/daos/UserDAO.java
+++ TomcatServer/tomcat-server/src/main/java/com/unipi/dsmt/app/daos/UserDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 
 import com.unipi.dsmt.app.entities.User;
 
@@ -34,17 +35,21 @@ public class UserDAO {
   }
 
   public String save(User userInfo) throws SQLException {
-    String sqlString = "INSERT INTO user(username, password, name, surname, department, onlineFlag, creationTime) VALUES (?, ?, ?, ?, ?, ?, ?)";
-    PreparedStatement statement = userConnection.prepareStatement(sqlString);
-    statement.setString(1, userInfo.getUsername());
-    statement.setString(2, userInfo.getPassword());
-    statement.setString(3, userInfo.getName());
-    statement.setString(4, userInfo.getSurname());
-    statement.setString(5, userInfo.getDepartment().toString());
-    statement.setBoolean(6, userInfo.getOnline_flag());
-    statement.setDate(7, userInfo.getCreationTime());
-    int changedCount = statement.executeUpdate();
-    return changedCount == 0 ? "user already exists" : "";
+    try {
+      String sqlString = "INSERT INTO user(username, password, name, surname, department, onlineFlag, creationTime) VALUES (?, ?, ?, ?, ?, ?, ?)";
+      PreparedStatement statement = userConnection.prepareStatement(sqlString);
+      statement.setString(1, userInfo.getUsername());
+      statement.setString(2, userInfo.getPassword());
+      statement.setString(3, userInfo.getName());
+      statement.setString(4, userInfo.getSurname());
+      statement.setString(5, userInfo.getDepartment().toString());
+      statement.setBoolean(6, userInfo.getOnline_flag());
+      statement.setDate(7, userInfo.getCreationTime());
+      int changedCount = statement.executeUpdate();
+      return changedCount == 0 ? "User already exists" : "";
+    } catch (SQLIntegrityConstraintViolationException e) {
+      return "User already exists";
+    }
   }
 
 }
--- TomcatServer/tomcat-server/src/main/java/com/unipi/dsmt/app/utils/ErrorHandler.java
+++ TomcatServer/tomcat-server/src/main/java/com/unipi/dsmt/app/utils/ErrorHandler.java
@@ -1,19 +1,11 @@
 package com.unipi.dsmt.app.utils;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class ErrorHandler {
   public static void safeDispatchToErrorPage(HttpServletRequest request, HttpServletResponse response, Exception e) {
     try {
-      StringWriter sw = new StringWriter();
-      PrintWriter pw = new PrintWriter(sw);
-      e.printStackTrace(pw);
-      request.setAttribute("message", e.getMessage().toString());
-      request.setAttribute("stack", sw.toString());
       System.out.println("[Server] -> An unexpected error has occurred:");
       e.printStackTrace();
       request.getRequestDispatcher("/WEB-INF/jsp/error_page.jsp").forward(request, response);
