6d9b28f26a575b736e78678f1eb9dd23e9c8b2ed
Fedeperrone98 <f.perrone6@studenti.unipi.it>
2023-10-14 11:16:15 +0200

    department page

---
+++ TomcatServer/tomcat-server/src/main/java/com/unipi/dsmt/app/endpoints/servlets/DepartmentServlet.java
@@ -0,0 +1,25 @@
+package com.unipi.dsmt.app.endpoints.servlets;
+
+import com.unipi.dsmt.app.utils.AccessController;
+import com.unipi.dsmt.app.utils.ErrorHandler;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "DepartmentServlet", value = "/department")
+public class DepartmentServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String token = AccessController.getToken(request);
+            if (token == null) {
+                request.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request, response);
+                return;
+            }
+        } catch (Exception e) {
+            ErrorHandler.safeDispatchToErrorPage(request, response, e);
+        }
+    }
+}
