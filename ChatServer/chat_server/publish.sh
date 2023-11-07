6f5920debd25cd8f34b9559e27acf7f8650f776c
Fedeperrone98 <f.perrone6@studenti.unipi.it>
2023-11-07 12:22:50 +0100

    detach

--- ChatServer/chat_server/publish.sh
+++ ChatServer/chat_server/publish.sh
@@ -1,3 +1,3 @@
 rebar3 clean
 rebar3 compile
-rebar3 shell --name 'chat@10.2.1.57' --setcookie 'messaging_server'
+nohup rebar3 shell --name 'chat@10.2.1.57' --setcookie 'messaging_server' &
--- NotificationServer/notification_server/publish.sh
+++ NotificationServer/notification_server/publish.sh
@@ -1,3 +1,3 @@
 rebar3 clean
 rebar3 compile
-rebar3 shell --name 'notification@10.2.1.59' --setcookie 'messaging_server'
+nohup rebar3 shell --name 'notification@10.2.1.59' --setcookie 'messaging_server' &
